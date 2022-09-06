cf8d1ed5e0c8468676731e4c9d7f035d1c89c108
Adam Sporczyk <adamspo00@gmail.com>
2022-09-06 19:34:12 +0200

    Initial Commit

---
+++ SzukajGry/SzukajGryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SzukajGryApp.swift
+//  SzukajGry
+//
+//  Created by Adam Sporczyk on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SzukajGryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
